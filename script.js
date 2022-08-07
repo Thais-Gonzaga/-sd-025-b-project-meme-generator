93874dc18a7109be200ce69af738d201ceb7282b
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-07 20:36:56 -0300

    adiciona requisito 6

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ const image = document.createElement('img');
 image.id = 'meme-image';
 conteiner.appendChild(image);
 
+const divButton = document.createElement('div');
+document.body.appendChild(divButton);
+
 function textInMeme() {
   p.innerText = input.value;
 }
@@ -30,10 +33,38 @@ function readFile() {
   const img = document.getElementById('meme-image');
   const memeInsert = document.getElementById('meme-insert');
   const fileReader = new FileReader();
-  console.log(fileReader);
   fileReader.onloadend = function change() {
     img.src = fileReader.result;
   };
   fileReader.readAsDataURL(memeInsert.files[0]);
 }
 inputFile.addEventListener('change', readFile);
+
+function createButton(id, text) {
+  const button = document.createElement('button');
+  button.id = id;
+  button.innerText = text;
+  divButton.appendChild(button);
+  return button;
+}
+const buttonRed = createButton('fire', 'Red');
+buttonRed.style.background = 'rgb(255, 0, 0)';
+const buttonBlue = createButton('water', 'Blue');
+buttonBlue.style.background = 'rgb(0, 0, 255)';
+const buttonGreen = createButton('earth', 'Green');
+buttonGreen.style.background = 'rgb(0, 128, 0)';
+
+function chargeBorderRed() {
+  conteiner.style.border = '3px dashed rgb(255, 0, 0)';
+}
+buttonRed.addEventListener('click', chargeBorderRed);
+
+function chargeBorderBlue() {
+  conteiner.style.border = '5px double rgb(0, 0, 255)';
+}
+buttonBlue.addEventListener('click', chargeBorderBlue);
+
+function chargeBorderGreen() {
+  conteiner.style.border = '6px groove rgb(0, 128, 0)';
+}
+buttonGreen.addEventListener('click', chargeBorderGreen);
