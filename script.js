44a628bc8c86b7554bc341f5c30633a9e888f607
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-07 19:21:45 -0300

    adiciona requisito 2,3 e 4

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+const input = document.createElement('input');
+input.id = 'text-input';
+input.type = 'text';
+// input.placeholder = 'Escreva aqui';
+input.max = 60;
+document.body.appendChild(input);
 const conteiner = document.createElement('div');
 conteiner.id = 'meme-image-container';
 document.body.appendChild(conteiner);
 
-const input = document.createElement('input');
-input.id = 'text-input';
-input.type = 'text';
-conteiner.appendChild(input);
+const inputFile = document.createElement('input');
+inputFile.type = 'file';
+inputFile.id = 'meme-insert';
+document.body.appendChild(inputFile);
 
-const p = document.createElement('p');
+const p = document.createElement('div');
 p.id = 'meme-text';
 conteiner.appendChild(p);
+
+const image = document.createElement('img');
+image.id = 'meme-image';
+conteiner.appendChild(image);
 
 function textInMeme() {
   p.innerText = input.value;
 }
 input.addEventListener('input', textInMeme);
+
+function readFile() {
+  const img = document.getElementById('meme-image');
+  const memeInsert = document.getElementById('meme-insert');
+  const fileReader = new FileReader();
+  console.log(fileReader);
+  fileReader.onloadend = function change() {
+    img.src = fileReader.result;
+  };
+  fileReader.readAsDataURL(memeInsert.files[0]);
+}
+inputFile.addEventListener('change', readFile);
